d76354a5022a8cb8c0a5c2fd7182f2641d18ce0b
qjawns9500 <61936724+qjawns9500@users.noreply.github.com>
2024-04-05 23:14:39 +0900

    경로 폴더 생성 및 메인 컨트롤러

---
+++ src/main/java/com/collab/controller/MainController.java
@@ -0,0 +1,9 @@
+package com.collab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MainController {
+
+}
