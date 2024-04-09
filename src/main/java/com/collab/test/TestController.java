45a839618c8e4a7f074037d37478ed5cfb907b7e
jungdaehun1024 <pooh99102408@gmail.com>
2024-04-10 00:19:31 +0900

    설정파일 구조 변경 + 오타수정

--- src/main/java/com/collab/test/TestController.java
+++ src/main/java/com/collab/test/TestController.java
@@ -2,14 +2,13 @@ package com.collab.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
 @RestController
-public class testController {
+public class TestController {
     @Autowired
     TestService testService;
 
